859bb6dd8f6ba0c44e4a0b268fb88548ad85f36b
meghamodi <meghamodi007@gmail.com>
2023-03-03 16:02:27 -0800

    feat: add navbar,footer,header,etc components

---
+++ src/components/Navbar.js
@@ -0,0 +1,64 @@
+import logo from '../images/logo.svg'
+import { pageLinks, socialLinks } from '../data'
+export function Navbar() {
+  return (
+    <>
+      <nav className='navbar'>
+        <div className='nav-center'>
+          <div className='nav-header'>
+            <img src={logo} className='nav-logo' alt='backroads' />
+            <button type='button' className='nav-toggle' id='nav-toggle'>
+              <i className='fas fa-bars'></i>
+            </button>
+          </div>
+          {/* <!-- left this comment on purpose --> */}
+          <ul className='nav-links' id='nav-links'>
+            {pageLinks.map(link => {
+              return (
+                <li key={link.id}>
+                  <a href={link.href} className='nav-link'>
+                    {link.text}
+                  </a>
+                </li>
+              )
+            })}
+          </ul>
+
+          <ul className='nav-icons'>
+            {socialLinks.map(socialLink => {
+              return (
+                <li key={socialLink.id}>
+                  <a
+                    href={socialLink.href}
+                    target='_blank'
+                    className='nav-icon'
+                  >
+                    <i className={socialLink.icon}></i>
+                  </a>
+                </li>
+              )
+            })}
+            {/* <li>
+              <a
+                href='https://www.twitter.com'
+                target='_blank'
+                className='nav-icon'
+              >
+                <i className='fab fa-twitter'></i>
+              </a>
+            </li>
+            <li>
+              <a
+                href='https://www.twitter.com'
+                target='_blank'
+                className='nav-icon'
+              >
+                <i className='fab fa-squarespace'></i>
+              </a>
+            </li> */}
+          </ul>
+        </div>
+      </nav>
+    </>
+  )
+}
